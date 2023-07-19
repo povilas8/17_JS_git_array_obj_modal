b2cbcc81ab4ac81f56b0b7ce04d6fce0178c06e4
Povilas8 <povilo@inbox.lt>
2023-07-19 08:53:46 +0300

    people array + html title + link js file

---
+++ js/arrayOfObj.js
@@ -0,0 +1,61 @@
+'use strict';
+console.log('arrayOfObj.js file was loaded');
+
+const people = [
+  {
+    name: 'Jonas',
+    surname: 'Jonaitis',
+    sex: 'male',
+    age: 26,
+    income: 1200,
+    married: false,
+    hasCar: false,
+  },
+  {
+    name: 'Severija',
+    surname: 'Piktutytė',
+    sex: 'female',
+    age: 26,
+    income: 1300,
+    married: false,
+    hasCar: true,
+  },
+  {
+    name: 'Valdas',
+    surname: 'Vilktorinas',
+    sex: 'male',
+    age: 16,
+    income: 0,
+    married: false,
+    hasCar: false,
+  },
+  {
+    name: 'Virginijus',
+    surname: 'Uostauskas',
+    sex: 'male',
+    age: 32,
+    income: 2400,
+    married: true,
+    hasCar: true,
+  },
+  {
+    name: 'Samanta',
+    surname: 'Uostauskienė',
+    sex: 'female',
+    age: 28,
+    income: 1200,
+    married: true,
+    hasCar: true,
+  },
+  {
+    name: 'Janina',
+    surname: 'Stalautinskienė',
+    sex: 'female',
+    age: 72,
+    income: 364,
+    married: false,
+    hasCar: false,
+  },
+];
+
+console.table(people);
